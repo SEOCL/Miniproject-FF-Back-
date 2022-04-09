11c80af780dfa6612ceb36ebf54f0534fbeb8e36
dongsun1 <le12352@naver.com>
2022-04-09 09:06:52 +0900

    큰 틀

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 connect();
 
-// const postsRouter = require("./routes/posts");
+// const exampleRouter = require("./routes/examples");
 
 const requestMiddleware = (req, res, next) => {
   console.log("Request URL:", req.originalUrl, " - ", new Date());
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(requestMiddleware);
 
-// app.use("/api", [usersRouter, postsRouter, commentsRouter, likesRouter]);
+// app.use("/api", );
 
 app.listen(port, () => {
   console.log(port, "포트로 서버가 켜졌어요!");
