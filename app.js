8f1963b6faa2ca12772824e115c8013c5153f5e8
qwe1234k <bbyl0124@gmail.com>
2022-04-09 17:48:26 +0900

    미들웨어 작업 - 태성

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const requestMiddleware = (req, res, next) => {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser());
 app.use(requestMiddleware);
 
 // 라우터 연결
