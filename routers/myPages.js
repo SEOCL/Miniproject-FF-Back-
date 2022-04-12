1d7a9822717c7f61a923ed1b295352f11ef4c071
dongsun1 <le12352@naver.com>
2022-04-12 09:21:39 +0900

    게시글 작성 시 이미지 업로드 기능 구현

--- routers/myPages.js
+++ routers/myPages.js
@@ -6,11 +6,11 @@ const Article = require("../schemas/article");
 const User = require("../schemas/user");
 const Like = require("../schemas/like");
 const authMiddleware = require("../middleware/authMiddleWare");
+const multipart = require("connect-multiparty");
+const multipartMiddleware = multipart();
 
 // 마이페이지 비밀번호 체크
 // 더미 데이터 테스트 완료 ##
-// 토큰 테스트 미완료
-// 인증미들웨어 추가예정
 router.post("/pwCheck", authMiddleware, async (req, res) => {
   // 글내용, 이미지URL, 카테고리
   const { userPw } = req.body;
@@ -28,8 +28,6 @@ router.post("/pwCheck", authMiddleware, async (req, res) => {
 
 // 내가 작성한 게시글 조회
 // 더미 데이터 테스트 완료 ##
-// 토큰 테스트 미완료
-// 인증미들웨어 추가예정
 router.get("/article", authMiddleware, async (req, res) => {
   try {
     // 글내용, 이미지URL, 카테고리
@@ -46,8 +44,6 @@ router.get("/article", authMiddleware, async (req, res) => {
 
 // 내가 좋아요 누른 게시글 조회
 // 더미 데이터 테스트 완료 ##
-// 토큰 테스트 미완료
-// 인증미들웨어 추가예정
 router.get("/articleLike", authMiddleware, async (req, res) => {
   try {
     // 글내용, 이미지URL, 카테고리
@@ -73,48 +69,29 @@ router.get("/articleLike", authMiddleware, async (req, res) => {
 
 // 프로필 이미지 업데이트
 // 더미 데이터 테스트 완료 ##
-// 토큰 테스트 미완료
-// 인증미들웨어 추가예정
+router.post(
+  "/profileUpdate",
+  multipartMiddleware,
+  authMiddleware,
+  async (req, res) => {
+    try {
+      // 프로필 이미지 URL, 유저id
+      const { user } = res.locals;
+      const userId = user.userId;
 
-// router.post("/profileUpdate", authMiddleware, async (req, res) => {
-//   try {
-//     // 프로필 이미지 URL, 유저id
-//     const { userProfile } = req.body;
-//     const { user } = res.locals;
-//     const userId = user.userId;
+      const { path } = req.files.null;
 
-//     await User.updateOne({ userId }, { $set: { userProfile } });
-//     res.status(200).json({ result: true });
-//   } catch (error) {
-//     console.log("myPages.js -> 프로필 이미지 업데이트에서 에러남");
-//     res.status(400).json({ result: false });
-//   }
-// });
-
-const multipart = require("connect-multiparty");
-const multipartMiddleware = multipart();
-
-router.post("/profileUpdate", multipartMiddleware, async (req, res) => {
-  try {
-    // 프로필 이미지 URL, 유저id
-    // const { user } = res.locals;
-    // const userId = user.userId;
-
-    const { userId } = req.body;
-    const { path } = req.files.null;
-
-    await User.updateOne({ userId }, { $set: { userProfile: path } });
-    res.status(200).json({ result: true });
-  } catch (error) {
-    console.log("myPages.js -> 프로필 이미지 업데이트에서 에러남");
-    res.status(400).json({ result: false });
+      await User.updateOne({ userId }, { $set: { userProfile: path } });
+      res.status(200).json({ result: true });
+    } catch (error) {
+      console.log("myPages.js -> 프로필 이미지 업데이트에서 에러남");
+      res.status(400).json({ result: false });
+    }
   }
-});
+);
 
 // 유저 닉네임 업데이트
 // 더미 데이터 테스트 완료 ##
-// 토큰 테스트 미완료
-// 인증미들웨어 추가예정
 router.post("/nameUpdate", authMiddleware, async (req, res) => {
   try {
     // 유저 닉네임, 유저id
--- routers/users.js
+++ routers/users.js
@@ -34,35 +34,9 @@ router.post("/logIn", async (req, res) => {
   //토큰 발급.
 });
 
-//로그아웃 api
-// user_id를 찾아서(auth를 통해 user의 정보에 들어있는 것) db에있는 토큰값을 비워준다.
-// 미들웨어 필요함.
-// 쿠키는 프론트에서 만들어준다.
-// 프론트 쪽에서 하기로 했으나 결정되진 않음.
-// router.get("/logout", (req, res) => {
-//     try {
-//         res.clearCookie('쿠키 이름')
-//     }
-//     //***리액트에서 보내주는 이름으로 수정해야 함
-
-//     catch (error) {
-
-//          res.status(400).json({ result: false })
-//     }
-
-// });
-
-//auth를 router.get("/logout", auth (req, res) =>) {} 테스트를 위해 뺐음.
-
 //유저가 회원가입 요청시 사용하는 API입니다.
 //썬더 클론 - 더미 데이터로 테스트 완료.
 router.post("/signUp", async (req, res) => {
-  //ID의 시작과 끝이 a-zA-Z0-9글자로 3 ~ 10 단어로 구성되어야 한다.
-  //Pw의 시작과 끝이 a-zA-Z0-9글자로 4 ~ 30 단어로 구성되어야 한다.
-  //id와 pw의 문자 입력 사항 제한은 프론트 쪽에서 하기로 했음.
-  // const re_userId = /^[a-zA-Z0-9]{3,10}$/;
-  // const re_userPw = /^[a-zA-Z0-9]{4,30}$/;
-
   let { userId, userPw, userName } = req.body;
 
   const userProfile = initProfile;
@@ -78,27 +52,6 @@ router.post("/signUp", async (req, res) => {
     });
     return;
   }
-  // } else if (userId.search(re_userId) == -1) {
-  //     console.log("아이디 형식 찾기에서 에러 발생")
-  //     res.status(400).send({
-  //         errorMessage: "아이디의 형식이 올바르지 않습니다."
-  //     });
-  //     return;
-
-  // } else if (userPw.search(re_userPw) == -1) {
-  //     console.log("패스워드 형식 찾기에서 에러 발생")
-  //     res.status(400).send({
-  //         errorMessage: "패스워드 형식이 올바르지 않습니다."
-  //     });
-  //     return;
-  // }
-  // else if (userPw.search(userId) != -1) {
-  //     console.log("패스워드에 아이디 포함에서 에러 발생")
-  //     res.status(400).send({
-  //         errorMessage: "패스워드에 아이디가 포함되어 있습니다."
-  //     });
-  //     return;
-  // }
 
   //회원 가입 성공 시의 메시지 호출.
   await User.create({ userId, userPw, userName, userProfile });
